fe4a7b9e0d6d95b017d905794ace1654a5696380
chenguang <chenguang0508@163.com>
2021-03-15 22:48:03 +0800

    优化

--- app/src/main/java/com/ld/coordl/RecyclerScrollActivity.java
+++ app/src/main/java/com/ld/coordl/RecyclerScrollActivity.java
@@ -2,13 +2,16 @@ package com.ld.coordl;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.ld.coordl.adapter.BuAdapter;
 import com.ld.coordl.entity.Bean;
+import com.ld.coordl.entity.ExpendItemEntity;
 import com.ld.coordl.entity.ItemEntry;
 import com.ld.coordl.entity.MoreViewEntry;
+import com.ld.coordl.entity.Type3Entity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +22,8 @@ import java.util.List;
 public class RecyclerScrollActivity extends AppCompatActivity {
     private List<ItemEntry> mTopDataList = new ArrayList<>();
     private List<ItemEntry> mDataList = new ArrayList<>();
-
     private RecyclerView mTopRecyclerView;
     private RecyclerView mRecyclerView;
-
     BuAdapter buAdapter;
     BuAdapter topAdapter;
 
@@ -30,22 +31,20 @@ public class RecyclerScrollActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_scrolling);
-        initView();
-        initData();
-    }
 
-    private void initView() {
         mTopRecyclerView = findViewById(R.id.recyclerview_top);
         mRecyclerView = findViewById(R.id.recyclerview);
 
         LinearLayoutManager topLinearLayoutManager = new LinearLayoutManager(this);
-        mTopRecyclerView.setLayoutManager(topLinearLayoutManager);
+        GridLayoutManager linearLayoutManager = new GridLayoutManager(this, 3);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+        mTopRecyclerView.setLayoutManager(topLinearLayoutManager);
+        initData();
     }
 
     private void initData() {
+        //top
         ItemEntry<MoreViewEntry> itemsEntry = new ItemEntry();
         itemsEntry.data = new MoreViewEntry();
         itemsEntry.floorType = FloorType.ITEM_TYPE_1;
@@ -56,21 +55,24 @@ public class RecyclerScrollActivity extends AppCompatActivity {
         itemsEntry2.floorType = FloorType.ITEM_TYPE_2;
         mTopDataList.add(itemsEntry2);
 
-        ItemEntry<Bean> itemsEntry3 = new ItemEntry();
-        itemsEntry3.data = new Bean();
+        ItemEntry<Type3Entity> itemsEntry3 = new ItemEntry();
+        itemsEntry3.data = new Type3Entity("item");
         itemsEntry3.floorType = FloorType.ITEM_TYPE_3;
         mTopDataList.add(itemsEntry3);
 
         topAdapter = new BuAdapter(this, mTopDataList);
         mTopRecyclerView.setAdapter(topAdapter);
 
-        for (int i = 0; i < 20; i++) {
-            mDataList.add(buildItemsEntry());
+        //content
+        for (int i = 0; i < 10; i++) {
+            ItemEntry<Type3Entity> itemsEntry0 = new ItemEntry();
+            itemsEntry0.data = new Type3Entity("item" + i);
+            itemsEntry0.floorType = FloorType.ITEM_TYPE_3;
+            mDataList.add(itemsEntry0);
         }
 
-
-        ItemEntry<Object> itemEntry4 = new ItemEntry();
-        itemEntry4.data = new Bean();
+        ItemEntry<ExpendItemEntity> itemEntry4 = new ItemEntry();
+        itemEntry4.data = new ExpendItemEntity();
         itemEntry4.floorType = FloorType.ITEM_TYPE_4;
         mDataList.add(0, itemEntry4);
 
@@ -78,10 +80,4 @@ public class RecyclerScrollActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(buAdapter);
     }
 
-    private ItemEntry<Bean> buildItemsEntry() {
-        ItemEntry<Bean> itemsEntry = new ItemEntry();
-        itemsEntry.data = new Bean();
-        itemsEntry.floorType = FloorType.ITEM_TYPE_3;
-        return itemsEntry;
-    }
 }
